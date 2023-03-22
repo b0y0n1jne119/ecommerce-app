9bedb2d5118bfef41528354e342129dc73a615bf
Minh Duc <nguyenminhduc119@gmail.com>
2023-03-22 08:16:56 +0700

    part 3

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,126 @@
 import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
+import { BsSearch } from 'react-icons/bs';
+import pic1 from '../assets/images/compare.svg';
+import pic2 from '../assets/images/wishlist.svg';
+import pic3 from '../assets/images/user.svg';
+import pic4 from '../assets/images/cart.svg';
 
 export default function Header(props) {
     return (
-        <h1>Header</h1>
+        <>
+            <header className='header-top-strip py-3'>
+                <div className='container'>
+                    <div className='row'>
+                        <div className='col-6'>
+                            <p className='text-white mb-0'>
+                                Free Shipping Over $100 & Free Returns
+                            </p>
+                        </div>
+                        <div className='col-6'>
+                            <p className='text-end text-white mb-0'>
+                                Hotline: <a href='tel:+84 337099460'>+84 3370 99460</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </header>
+            <header className='header-upper py-3'>
+                <div className='container'>
+                    <div className='row align-items-center'>
+                        <div className='col-2'>
+                            <h2>
+                                <Link className='text-white'>Dev V</Link>
+                            </h2>
+                        </div>
+                        <div className='col-5'>
+                            <div className="input-group">
+                                <input
+                                    type="text"
+                                    className="form-control py-2"
+                                    placeholder="Search Product Here"
+                                    aria-label="Search Product Here"
+                                    aria-describedby="basic-addon2" />
+                                <span className="input-group-text p-3" id="basic-addon2">
+                                    <BsSearch className='fs-6' />
+                                </span>
+                            </div>
+                        </div>
+                        <div className='col-5'>
+                            <div className='header-upper-links d-flex align-items-center justify-content-between'>
+                                <div>
+                                    <Link className='d-flex align-items-center gap-10 text-white'>
+                                        <img src={pic1} alt="compare" />
+                                        <p className='mb-0'>
+                                            Compare <br /> Products
+                                        </p>
+                                    </Link>
+                                </div>
+                                <div>
+                                    <Link className='d-flex align-items-center gap-10 text-white'>
+                                        <img src={pic2} alt="wishlist" />
+                                        <p className='mb-0'>
+                                            Favourite <br /> wishlist
+                                        </p>
+                                    </Link>
+                                </div>
+                                <div>
+                                    <Link className='d-flex align-items-center gap-10 text-white'>
+                                        <img src={pic3} alt="user" />
+                                        <p className='mb-0'>
+                                            Log in <br /> My Account
+                                        </p>
+                                    </Link>
+                                </div>
+                                <div>
+                                    <Link className='d-flex align-items-center gap-10 text-white'>
+                                        <img src={pic4} alt="cart" />
+                                        <div className='d-flex flex-column gap-10'>
+                                            <span className='badge bg-white text-dark'>0 </span>
+                                            <p className='mb-0'>$ 500</p>
+                                        </div>
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </header>
+            <header className='header-bottom py-3'>
+                <div className='container'>
+                    <div className='row'>
+                        <div className='col-12'>
+                            <div className='menu-bottom d-flex align-items-center gap-30'>
+                                <div>
+                                    <div className="dropdown">
+                                        <button
+                                            className="btn btn-secondary dropdown-toggle bg-transparent"
+                                            type="button"
+                                            id="dropdownMenuButton1"
+                                            data-bs-toggle="dropdown"
+                                            aria-expanded="false">
+                                            Shop Categories
+                                        </button>
+                                        <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                                            <li><a class="dropdown-item" href="#">Action</a></li>
+                                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div className='menu-links'>
+                                    <div className='d-flex align-items-center gap-15'>
+                                        <NavLink to="/">Home</NavLink>
+                                        <NavLink to="/">Our Store</NavLink>
+                                        <NavLink to="/">Blogs</NavLink>
+                                        <NavLink to="/contact">Contact</NavLink>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </header>
+        </>
     )
 }
